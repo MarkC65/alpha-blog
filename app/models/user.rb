01038b1c7b85518cc43f778a1179a92d6c901c25
MarkC65 <mark.chalinder@gmail.com>
2020-04-26 13:13:40 +0100

    Add secure password to Users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
     format: { with: VALID_EMAIL_REGEX, message: "not a valid email address" }
-end
+  has_secure_password
+  end
