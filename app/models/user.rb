90800dd3b447a9144049c20cc71e962e4ee21ea3
MarkC65 <mark.chalinder@gmail.com>
2020-04-26 10:20:14 +0100

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 3..25 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
+    format: { with: VALID_EMAIL_REGEX, message: "not a valid email address" }
 end
