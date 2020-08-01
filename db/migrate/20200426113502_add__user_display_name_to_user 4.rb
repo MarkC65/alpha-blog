42cacc708de70520d67d5fb25b850e4bfa4ff93e
MarkC65 <mark.chalinder@gmail.com>
2020-08-01 22:03:25 +0100

    Resolve security issue with elliptic@9.5.2

--- db/migrate/20200426113502_add__user_display_name_to_user 4.rb
+++
@@ -1,5 +0,0 @@
-class AddUserDisplayNameToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :display_name, :string
-  end
-end
--- db/migrate/20200426120228_add_password_digest_to_users_table 4.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordDigestToUsersTable < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :password_digest, :string
-  end
-end
